b16454a32ccde59b70edce226dd2d1ee0cfbba6d
cbowen <cbowen@cse.msu.edu>
2019-01-06 13:05:36 -0500

    New cl-publish system.

---
+++ publish.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+	dist: path.resolve(__dirname, 'dist'),
+	files: [
+		'Review\\.js'
+	]
+}
--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,7 @@
 const path = require('path');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
 	entry: {
 		Review: path.resolve(__dirname, 'index.js')
-	},
-	plugins: [
-		new FileManagerPlugin({
-			onEnd: {
-				copy: [
-					{source: path.resolve(__dirname, '../../../cl/dist/review.*'), destination: path.resolve(__dirname, 'dist') }
-				]
-			}
-		})
-	]
+	}
 }
