cac8254f91fc69954333a29d034f05de6f240428
rodsan0 <rodsan0@users.noreply.github.com>
2023-03-24 02:20:48 -0400

    Add menu to select annotations

--- js/ReviewsVue.vue
+++ js/ReviewsVue.vue
@@ -2,21 +2,38 @@
   <div class="cl-reviews">
     <h3>Reviews of this assignment appear here.</h3>
     <p class="cl-reviews-none" v-if="reviews.length === 0">*** None Yet ***</p>
+    <table class="small">
+      <tr>
+        <th>Review Annotations</th>
+      </tr>
+      <tr v-for="(review, index) in reviews">
+        <td><review-annotation :id="index" :time="review.time" :author="review.by" :svg="review.annotation" :width="review.annotation_width" :height="review.annotation_height" :submission_id="Object.values(review.submissions)[0].id" v-on:show="show"></review-annotation></td>
+        <!-- <td>{{index + 1}}th review {{formatTime(review.time)}} by {{review.by}}</td> -->
+      </tr>
+    </table>
+    <div v-if="shownAnnotation !== null">
+      <figure v-if="previewImg !== null" class="cl-preview">
+        <div class="cl-review-diagram">
+          <img ref="diagramImage" :src="shownAnnotation.image_src">
+          <svg xmlns="http://www.w3.org/2000/svg" v-html="shownAnnotation.svg" :height="shownAnnotation.height" :width="shownAnnotation.width"></svg>
+        </div>
+      </figure>
+    </div>
+
+
     <div v-for="review in reviews" class="cl-review">
       <h3 :class="review.role !== undefined ? 'staff' : ''">{{formatTime(review.time)}}
         <span v-if="review.role !== undefined">Staff Review</span>
         <span v-else>Review</span> by {{review.by}}
         <span class="cl-submitted">{{showSubmissions(review)}}</span></h3>
-        <div class="cl-review-diagram">
-          <img ref="diagramImage" :src="previewImg(review.submissions)">
-          <svg xmlns="http://www.w3.org/2000/svg" v-html="review.annotation" :height="review.annotation_height" :width="review.annotation_width"></svg>
-        </div>
       <div class="cl-review-present">{{review.review}}</div>
     </div>
   </div>
 </template>
 
 <script>
+
+import ReviewAnnotationVue from './ReviewAnnotationVue.vue';
 /**
  * This is the inline vue for reviews of an assignment used by the
  * submission and page the course console grading page.
@@ -28,8 +45,12 @@ export default {
   data: function () {
     return {
       assignTag: '',
-      reviews: []
+      reviews: [],
+      shownAnnotation: null,
     }
+  },
+  components: {
+    reviewAnnotation: ReviewAnnotationVue
   },
   mounted() {
     this.assignTag = this.json.assignTag;
@@ -54,9 +75,14 @@ export default {
 
       return 'Submission: ' + ret;
     },
-    previewImg(submissions) {
-      return this.$site.root + '/cl/course/submission/view/' + Object.values(submissions)[0].id;
-    },
+    show(svg, width, height, image_src) {
+      this.shownAnnotation = {
+        svg: svg,
+        width: width,
+        height: height,
+        image_src: image_src
+      };
+    }
   }
 }
 </script>
