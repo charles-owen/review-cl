80c642394fe0971a1914be818eca4624d7bfef0f
rodsan0 <rodsan0@users.noreply.github.com>
2023-03-30 04:40:15 -0400

    Remove unused code from ReviewsVue

--- js/ReviewsVue.vue
+++ js/ReviewsVue.vue
@@ -2,36 +2,6 @@
   <div class="cl-reviews">
     <h3>Reviews of this assignment appear here.</h3>
     <p class="cl-reviews-none" v-if="reviews.length === 0">*** None Yet ***</p>
-    <table class="small">
-      <tr>
-        <th>Review Annotations</th>
-      </tr>
-      <tr v-for="(review, index) in reviews">
-        <td><review-annotation :id="index" :time="review.time" :author="review.by" :svg="review.annotation" :width="review.annotation_width" :height="review.annotation_height" :submission_id="Object.values(review.submissions)[0].id" v-on:show="show"></review-annotation></td>
-        <!-- <td>{{index + 1}}th review {{formatTime(review.time)}} by {{review.by}}</td> -->
-      </tr>
-    </table>
-    <div v-if="shownAnnotation !== null">
-      <figure v-if="previewImg !== null" class="cl-preview">
-        <div class="cl-review-diagram">
-          <img ref="diagramImage" :src="shownAnnotation.image_src">
-          <svg xmlns="http://www.w3.org/2000/svg" v-html="shownAnnotation.svg" :height="shownAnnotation.height" :width="shownAnnotation.width"></svg>
-        </div>
-      </figure>
-    </div>
-
-
-    <div v-for="review in reviews" class="cl-review">
-      <h3 :class="review.role !== undefined ? 'staff' : ''">{{formatTime(review.time)}}
-        <span v-if="review.role !== undefined">Staff Review</span>
-        <span v-else>Review</span> by {{review.by}}
-        <span class="cl-submitted">{{showSubmissions(review)}}</span></h3>
-      <div class="cl-review-present">
-        <review-annotation :id="index" :time="review.time" :author="review.by" :svg="review.annotation" :width="review.annotation_width" :height="review.annotation_height" :submission_id="Object.values(review.submissions)[0].id" v-on:show="show">
-          {{review.review}}
-        </review-annotation>
-      </div>
-    <p class="cl-reviews-none" v-if="reviewing.length === 0">*** None Yet ***</p>
     <div v-for="id in json.ids" class="cl-review">
       <review-chat :json="json" :context="context" :chat_id="id"></review-chat>
     </div>
@@ -40,7 +10,6 @@
 
 <script>
 
-import ReviewAnnotationVue from './ReviewAnnotationVue.vue';
 import ReviewChatVue from './ReviewChat.vue'
 
 /**
@@ -61,7 +30,6 @@ export default {
     }
   },
   components: {
-    reviewAnnotation: ReviewAnnotationVue,
     reviewChat: ReviewChatVue,
   },
   mounted() {
